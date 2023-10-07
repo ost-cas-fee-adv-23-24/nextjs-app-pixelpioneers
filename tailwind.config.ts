06e1e0f291da49ace15d66028c6ce39de7ecd522
André Ceres <andre.ceres@mgb.ch>
2023-10-07 20:11:44 +0200

    feat(bind-library-add-button): install library
    - install library system design
    - import the library
    - add button
    - add button link
    - add ignore pack file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  // TODO: Later we also need to prepare the preset stuff, speaks to System Token / Token ID
+  // presets: [require('@ost-cas-fee-adv-23-24/design-system-pixelpioneers/preset')],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@ost-cas-fee-adv-23-24/design-system-pixelpioneers/lib/esm/components/**/*.js',
   ],
   theme: {
     extend: {
