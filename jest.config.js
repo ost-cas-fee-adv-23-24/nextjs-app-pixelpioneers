a00cc064db3ad538bdaf23cc82d3dc438db9f203
Nico Lutz <nico.lutz@ost.ch>
2023-10-09 15:39:55 +0200

    chore: try to add jest react-testing-library setup

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+//import {defaults} from 'jest-config';
+
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+    moduleFileExtensions: ['js', 'ts', 'tsx'],
+    //moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jest-environment-jsdom',
+    testMatch: ['**/tests/unit/**/*.ts?(x)', '**/tests/snapshot/**/*.ts?(x)'],
+    verbose: true,
+    preset: 'jest-playwright-preset',
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
---
+++ tests/snapshot/test.test.tsx
@@ -0,0 +1,14 @@
+import {Button} from '@ost-cas-fee-adv-23-24/design-system-pixelpioneers';
+import {render} from '@testing-library/react'
+
+describe('Home', () => {
+    it('renders a heading', () => {
+
+        const button = render(<Button label='hey'/>);
+
+
+        expect('hey').toBeInTheDocument();
+
+        expect(button).toMatchSnapshot();
+    })
+})
