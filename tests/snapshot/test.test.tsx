d9f73249e44ff0b31e0d85ff6aae7d919e3f11d1
André Ceres <andre.ceres@mgb.ch>
2024-01-18 15:01:49 +0100

    feat(authentication-zitadel): fix test jest

--- tests/snapshot/test.test.tsx
+++ tests/snapshot/test.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Button } from '@ost-cas-fee-adv-23-24/design-system-pixelpioneers';
+import {
+    Button,
+    ButtonSize,
+    IconProfile,
+} from '@ost-cas-fee-adv-23-24/design-system-pixelpioneers';
 import { render } from '@testing-library/react';
 
 describe('Home', () => {
     it('renders a heading', () => {
-        const button = render(<Button label="hey" />);
+        const button = render(
+            <Button label="Login with Zitadel" Icon={IconProfile} size={ButtonSize.M} />,
+        );
 
         // expect('hey').toBeInTheDocument();
 
