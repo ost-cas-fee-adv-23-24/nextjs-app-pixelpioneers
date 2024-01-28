0a7c134f0c00940893b3c90040cfe4d032aa2a35
Nico Lutz <nico.lutz@ost.ch>
2024-01-28 17:10:45 +0100

    fix: retype token to any

--- app/api/auth/[...nextauth]/auth.ts
+++ app/api/auth/[...nextauth]/auth.ts
@@ -5,6 +5,9 @@ export const {
     handlers: { GET, POST },
     auth,
 } = NextAuth({
+    session: {
+        strategy: 'jwt',
+    },
     providers: [
         ZITADEL({
             clientId: process.env.ZITADEL_CLIENT_ID,
@@ -30,7 +33,9 @@ export const {
             }
             return token;
         },
-        session({ session, token }) {
+        // FIXME: type correctly
+        // eslint-disable-next-line
+        session({ session, token }: any) {
             session.accessToken = token.accessToken;
             session.user = token.user;
             return session;
