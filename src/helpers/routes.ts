6b2542d28ecc66a9380eb15d2a5b1b4d150d2e95
Nico Lutz <nico.lutz@ost.ch>
2024-02-09 13:17:49 +0100

    feat: refactor api routes

--- src/helpers/routes.ts
+++ src/helpers/routes.ts
@@ -1,5 +1,24 @@
-export enum PAGE_ROUTES {
-    HOME = '/',
-    USERS = '/users/:id',
-    POSTS = '/posts/:id',
+export enum APP_ROUTES {
+    POST = '/post/[id]',
+    PROFILE = '/profile/[id]',
+    // TODO: Followers, login & settings ?
+    PROFILE_FOLLOWERS = '/profile/[id]/followers',
+    SETTINGS = '/settings',
+    LOGIN = '/login',
+    MAIN = '/',
 }
+
+export enum API_ROUTES {
+    POSTS = '/posts',
+    POSTS_ID = '/posts/[id]',
+    POSTS_ID_LIKES = '/posts/[id]/likes',
+    POSTS_ID_REPLIES = '/posts/[id]/replies',
+    POSTS_ID_MEDIA = '/posts/[id]/media',
+    POSTS_ID_FOLLOWERS = '/users/[id]/followers',
+    POSTS_REAL_TIME_DATA = '/posts/_sse',
+    USERS = '/users',
+    USERS_ID = '/users/[id]',
+    USERS_AVATAR = '/posts/avatar',
+}
+
+export const getRoute = (route: APP_ROUTES | API_ROUTES, id = '') => route.replace('[id]', id);
