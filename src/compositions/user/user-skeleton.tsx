d1e19961d28698615753d836639f226685b2774e
André Ceres <andre.ceres@mgb.ch>
2024-02-23 15:36:36 +0100

    feat(profil-page): optimze user skeleton

--- src/compositions/user/user-skeleton.tsx
+++ src/compositions/user/user-skeleton.tsx
@@ -8,12 +8,12 @@ export default function SkeletonUser() {
     // TODO: Need to evaluate skeleton
     return (
         <>
-            <section className="flex h-[220px] w-[200px] items-center justify-around rounded-m border-slate-300 md:content-center">
+            <section className="flex h-[220px] w-[216px] items-center justify-around rounded-m border-slate-300 md:content-center">
                 <div className="flex h-full w-full animate-pulse flex-col items-center justify-center space-y-m p-m">
                     <div className="h-xl w-xl rounded-full bg-slate-300 "></div>
-                    <div className={clsx(skeletonText, 'h-[8px]')}></div>
-                    <div className={clsx(skeletonText, 'h-[8px]')}></div>
-                    <div className={clsx(skeletonText, 'h-[30px]')}></div>
+                    <div className={clsx(skeletonText, 'h-[12px]')}></div>
+                    <div className={clsx(skeletonText, 'h-[8px] w-1/3')}></div>
+                    <div className={clsx(skeletonText, 'h-[30px] w-1/2')}></div>
                 </div>
             </section>
 
