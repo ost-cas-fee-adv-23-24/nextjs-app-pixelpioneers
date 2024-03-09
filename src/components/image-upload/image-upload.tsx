bcc56ab0878dfaf05d823c8d54fce211fd44904d
André Ceres <andre.ceres@mgb.ch>
2024-03-09 16:13:35 +0100

    feat(upload-image): add status by uploading any file

--- src/components/image-upload/image-upload.tsx
+++ src/components/image-upload/image-upload.tsx
@@ -1,20 +1,65 @@
 'use client';
 
-import { FileUpload, IconUpload } from '@ost-cas-fee-adv-23-24/design-system-pixelpioneers';
+import {
+    FileUpload,
+    IconCancel,
+    IconCheckmark,
+    IconUpload,
+    Label,
+    LabelSize,
+} from '@ost-cas-fee-adv-23-24/design-system-pixelpioneers';
+import clsx from 'clsx';
+import { useState } from 'react';
+
+// const isImage = (fileName: string): boolean => {
+//     const regEx = new RegExp(/[^\s]+(.*?).(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$/);
+
+//     if (fileName === null) {
+//         return false;
+//     }
+
+//     return regEx.test(fileName) ? true : false;
+// };
 
 export default function ImageUpload() {
-    const setFile = (file: File) => {
-        // eslint-disable-next-line no-console
-        console.log(file);
+    const [isImage, setIsImage] = useState('');
+
+    const setFile = (file: File): void => {
+        // isImageType = isImage(file.name);
+        setIsImage(file.name);
     };
 
+    const fileStatusText = clsx('self-center');
+
     return (
-        <FileUpload
-            maxFileSizeUpload={52428800}
-            Icon={IconUpload}
-            label="Datei hierhin ziehen..."
-            labelButton="... oder Datei auswählen"
-            onLoadFile={setFile}
-        />
+        <>
+            <FileUpload
+                maxFileSizeUpload={52428800}
+                Icon={IconUpload}
+                label="Datei hierhin ziehen..."
+                labelButton="... oder Datei auswählen"
+                onLoadFile={setFile}
+            />
+            <div className="flex flex-row self-center">
+                {isImage ? (
+                    <>
+                        <IconCheckmark className="mr-xs fill-primary-600" />
+                        <Label
+                            size={LabelSize.S}
+                            className={clsx(fileStatusText, 'text-primary-600')}
+                        >
+                            Bild gewählt
+                        </Label>
+                    </>
+                ) : (
+                    <>
+                        <IconCancel className="mr-xs fill-error" />
+                        <Label size={LabelSize.S} className={clsx(fileStatusText, 'text-error')}>
+                            Kein Bild gewählt
+                        </Label>
+                    </>
+                )}
+            </div>
+        </>
     );
 }
