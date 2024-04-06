7db851ae14922d64d645b6106398beea4b9885f3
André Ceres <andre.ceres@mgb.ch>
2024-04-06 16:00:06 +0200

    feat(loading-skeleton): rename label text for removing image

--- src/compositions/post/post-form.tsx
+++ src/compositions/post/post-form.tsx
@@ -98,7 +98,7 @@ export default function PostForm({
                             Icon={IconCancel}
                             variant={Variant.PRIMARY}
                             fill
-                            label="Bildvorschau löschen"
+                            label="Bild löschen"
                         />
                     </>
                 )}
