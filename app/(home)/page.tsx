983a6e35d6bb0a8c8a1fcd276fe2d0f7f38ebc86
André Ceres <andre.ceres@mgb.ch>
2024-04-09 20:26:42 +0200

    feat(loading-skeleton): remove unneeded skeleton

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { auth } from '@/app/api/auth/[...nextauth]/auth';
 import PostForm from '@/src/compositions/post/post-form';
 import { Heading, HeadingLevel } from '@ost-cas-fee-adv-23-24/design-system-pixelpioneers';
@@ -6,7 +6,6 @@ import { getUser } from '../actions/user';
 import { MessageVariant } from '@/src/compositions/post/types';
 import { createPost } from '@/app/actions/post';
 import clsx from 'clsx';
-import LoadingPost from './loading';
 
 export default async function Home() {
     const session = await auth();
@@ -26,17 +25,15 @@ export default async function Home() {
                     dicta.
                 </Heading>
             </header>
-            <Suspense fallback={<LoadingPost />}>
-                {user && (
-                    <section className={clsx(mainContainerClasses, 'flex w-full flex-col gap-y-m')}>
-                        <PostForm
-                            user={user}
-                            messageVariant={MessageVariant.POST}
-                            onCreate={createPost}
-                        />
-                    </section>
-                )}
-            </Suspense>
+            {user && (
+                <section className={clsx(mainContainerClasses, 'flex w-full flex-col gap-y-m')}>
+                    <PostForm
+                        user={user}
+                        messageVariant={MessageVariant.POST}
+                        onCreate={createPost}
+                    />
+                </section>
+            )}
         </>
     );
 }
