404136496837753467460d6d1ca842bb736b3cc7
André Ceres <andre.ceres@mgb.ch>
2024-05-02 16:16:17 +0200

    feat(mock-server-setup): add wait time for timout

--- tests/e2e/04-check-content-in-list-of-posts.test.ts
+++ tests/e2e/04-check-content-in-list-of-posts.test.ts
@@ -4,6 +4,9 @@ import { urlTestApp } from '../utils';
 test('Check the text content in the home route', async ({ page }) => {
     await page.goto(urlTestApp);
 
+    // Wait for a response from the /api/posts endpoint
+    await page.waitForTimeout(5000);
+
     expect(await page.textContent('body')).toContain('claudio');
     expect(await page.textContent('body')).toContain('hello from API 2');
 });
