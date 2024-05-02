0635d69e9cd26d08b67aa951265f7bd473ab715a
André Ceres <andre.ceres@mgb.ch>
2024-05-02 18:06:34 +0200

    feat(mocks-server-setup): a try with wait for mock server

--- tests/e2e/04-check-content-in-list-of-posts.test.ts
+++ tests/e2e/04-check-content-in-list-of-posts.test.ts
@@ -2,10 +2,8 @@ import { expect, test } from '@playwright/test';
 import { urlTestApp } from '../utils';
 
 // TODO: Need to check why this is not working on GitHub pipeline
-test.skip('Check the text content in the home route', async ({ page }) => {
+test.only('Check the text content in the home route', async ({ page }) => {
     await page.goto(urlTestApp);
-
-    // Wait for a response from the /api/posts endpoint
     await page.waitForTimeout(5000);
 
     expect(await page.textContent('body')).toContain('claudio');
