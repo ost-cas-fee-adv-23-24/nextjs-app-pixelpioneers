0fc434f1c9c6cf34e228035dbe8d5deaf832856a
André Ceres <andre.ceres@mgb.ch>
2024-05-06 15:06:05 +0200

    feat(mocks-server-setup): fix list check

--- tests/e2e/07-check-count-list.test.ts
+++ tests/e2e/07-check-count-list.test.ts
@@ -7,8 +7,6 @@ test.describe('should have the list of 18 posts ', () => {
 
         await page.waitForTimeout(4000);
 
-        const testPostMessage = await page.$$('[data-testid="testPostMessage"]');
-
-        expect(testPostMessage.length).toBe(18);
+        await expect(page.getByTestId('testPostMessage')).toHaveCount(18);
     });
 });
