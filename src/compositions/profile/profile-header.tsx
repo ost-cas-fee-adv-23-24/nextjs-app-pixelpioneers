ae247fd677ad4aeeb453f22d4af7ac8c4072a8ee
Nico Lutz <nico.lutz@ost.ch>
2024-05-08 11:00:33 +0200

    fix: add modal improvements

--- src/compositions/profile/profile-header.tsx
+++ src/compositions/profile/profile-header.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import ImageHeader from '@/src/components/image-header/image-header';
 import DisplayName from '@/src/compositions/display-name/display-name';
 import { DisplayNameVariant } from '@/src/compositions/display-name/types';
@@ -11,23 +13,32 @@ type ProfileHeaderProps = {
 };
 export default function ProfileHeader({ user, isActiveUser = false }: ProfileHeaderProps) {
     return (
-        <section className="flex flex-col gap-m">
-            <ImageHeader user={user} isActiveUser={isActiveUser} />
-            <section className="mx-m flex flex-col gap-base md:mx-0 md:w-container">
-                <DisplayName
-                    user={user}
-                    variant={DisplayNameVariant.PROFILE}
-                    isActiveUser={isActiveUser}
-                />
-                <Paragraph
-                    className="w-fill text-slate-400"
-                    size={ParagraphSize.M}
-                    title={`Über ${user.username}`}
-                >
-                    Willkommen auf {user.username}s Seite! Hätte er oder sie eine Bio verfasst,
-                    könntest du diese hier lesen.
-                </Paragraph>
+        <>
+            <section className="flex flex-col gap-m">
+                <ImageHeader user={user} isActiveUser={isActiveUser} />
+                <section className="mx-m flex flex-col gap-base md:mx-0 md:w-container">
+                    <DisplayName
+                        user={user}
+                        variant={DisplayNameVariant.PROFILE}
+                        isActiveUser={isActiveUser}
+                    />
+                    <Paragraph
+                        className="w-fill text-slate-400"
+                        size={ParagraphSize.M}
+                        title={`Über ${user.username}`}
+                    >
+                        Willkommen auf {user.username}s Seite! Hätte er oder sie eine Bio verfasst,
+                        könntest du diese hier lesen.
+                    </Paragraph>
+                </section>
             </section>
-        </section>
+            {/*<ModalImageUpload
+                onChange={() => console.log('e')}
+                inputRef={undefined}
+                isOpen={true}
+                onSubmit={() => console.log('e')}
+                onCancel={() => console.log('e')}
+            />*/}
+        </>
     );
 }
